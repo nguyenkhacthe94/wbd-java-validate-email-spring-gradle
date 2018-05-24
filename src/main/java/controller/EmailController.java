f8f1a94cb6e41e4332191cd2dc8b4fd9cef4b68e
Himedere <nguyenkhacthe94@gmail.com>
2018-05-24 16:19:00 +0700

    email validate

---
+++ src/main/java/controller/EmailController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import com.sun.org.apache.regexp.internal.RE;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Controller
+public class EmailController {
+    private static Pattern pattern;
+    private Matcher matcher;
+
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+
+    public EmailController() {
+        pattern = Pattern.compile(EMAIL_REGEX);
+    }
+
+    @GetMapping("/") String getIndex () {
+        return "index";
+    }
+
+    @PostMapping ("/validate") String validateEmail (@RequestParam("email") String email, Model model) {
+        boolean isValid = this.validate(email);
+        if (!isValid) {
+            model.addAttribute("message", "INVALID EMAIL");
+            return "index";
+        }
+
+        model.addAttribute("message", "VALIDATE SUCCESS");
+        return "index";
+    }
+
+    private boolean validate(String regex) {
+        matcher = pattern.matcher(regex);
+        return matcher.matches();
+    }
+}
